1e16b50b7762aae0695068d95cb88dfe7418e0ad
zhanglue <zhanglue@gmail.com>
2023-01-13 12:19:23 +0800

    Updated demo.

--- src/GrpcServiceDemo/Program.cs
+++ src/GrpcServiceDemo/Program.cs
@@ -1,25 +1,17 @@
+using GrpcServiceDemo.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddGrpc();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
+app.MapGrpcService<GreeterService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
