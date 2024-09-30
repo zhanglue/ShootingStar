c12c6f616ccaed68317e73dd932cc483b8f7f423
ZhangLue <2647332+zhanglue@users.noreply.github.com>
2024-09-30 20:36:38 +0800

    ReSetup Azure Kubernetes. (#19)
    * Upgrade version of github action steps to latest.
    
    * Upgrade tasks in VS Code.
    
    * Set kubectl version.
    
    * Try to debug the error in grpc client.
    
    * Switch another base image.
    
    * Add more info to debug.
    
    * Codestyle.

--- tools/check_grpc_client_response.sh
+++ tools/check_grpc_client_response.sh
@@ -19,6 +19,7 @@ if [[ $pod_cnt == 0 ]]; then
     exit 1
 elif [[ $pod_cnt != 1 ]]; then
     echo "Pod counts of grpc-client-demo is: ${pod_cnt}."
+    kubectl get pods --namespace ${pod_namespace} 2>/dev/null | grep -- ${pod_prefix} | sed 's/[[:space:]]\+/ /g'
     exit 1
 fi
 
@@ -28,18 +29,28 @@ pod_status=$(echo ${pod_info} | cut -d ' ' -f 3)
 echo "Pod status: ${pod_status}."
 if [[ ${pod_status} != 'Completed' ]]; then
     echo "Pod status is not Completed."
+
+    echo "Logs of the pod:"
+    pod_name=$(kubectl get pods --namespace ${pod_namespace} 2>/dev/null | grep -- ${pod_prefix} | sed 's/[[:space:]]\+/ /g' | cut -d ' ' -f 1 | tail -n 1)
+    kubectl logs --namespace ${pod_namespace} ${pod_name}
+
     exit 2
 fi
 
 pod_name=$(echo ${pod_info} | cut -d ' ' -f 1)
 actual_response=$(kubectl logs --namespace ${pod_namespace} --tail 1 ${pod_name})
-echo "Expected response: ${expected_response}"
-echo "Actual response: ${actual_response}"
+echo "Expected response : ${expected_response}"
+echo "Actual response   : ${actual_response}"
 
 echo ""
 echo "################################################################################"
 if [[ ${actual_response} != ${expected_response} ]]; then
     echo "!!!FAILED!!!"
+
+    echo "Logs of the pod:"
+    pod_name=$(kubectl get pods --namespace ${pod_namespace} 2>/dev/null | grep -- ${pod_prefix} | sed 's/[[:space:]]\+/ /g' | cut -d ' ' -f 1 | tail -n 1)
+    kubectl logs --namespace ${pod_namespace} ${pod_name}
+
     exit 3
 fi
 
