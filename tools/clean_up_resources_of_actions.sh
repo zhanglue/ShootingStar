c4da1de1521cbabc6c6cc0c2b4b07e63d5c266bc
ZhangLue <2647332+zhanglue@users.noreply.github.com>
2024-10-16 18:31:43 +0800

    Remove K8s resource before ACR image. (#24)

--- tools/clean_up_resources_of_actions.sh
+++ tools/clean_up_resources_of_actions.sh
@@ -18,20 +18,6 @@ if [[ $* =~ "--show-account" ]]; then
     fi
 fi
 
-# clean up docker images
-az acr repository show-tags --name shootingstar --repository grpc-service-demo --output table > /dev/null 2>&1
-if [[ $? == 0 ]]; then
-    echo_info "All docker images to delete:"
-    az acr repository show-tags --name shootingstar --repository grpc-service-demo --output table | tail -n +3 | awk '{print $1}' | grep "^pr-" | while read tag
-    do
-        echo_warning "grpc-service-demo:${tag}"
-        az acr repository delete --name shootingstar --image grpc-service-demo:${tag} --yes
-
-        echo_warning "grpc-client-demo:${tag}"
-        az acr repository delete --name shootingstar --image grpc-client-demo:${tag} --yes
-    done
-fi
-
 # clean up kubernetes resources
 pod_namespace_prefix=${1:-grpc-demo}
 
@@ -43,24 +29,35 @@ fi
 
 echo "################################################################################"
 echo_info "All kubernetes namespace to delete:"
-kubectl get namespaces | grep ^${pod_namespace_prefix} | sed 's/[[:space:]]\+/ /g' | cut -d ' ' -f 1
-echo ""
-
-echo -n "Are you sure to delete all the above namespaces? (y/N)"
-read -r confirm
-if [[ ! ${confirm} =~ ^[Yy]$ ]]; then
-    echo_info "Exit without deleting."
-    exit 0
-fi
-
-echo ""
-echo "################################################################################"
-echo_info "Deleting namespaces:"
 kubectl get namespaces | grep ^${pod_namespace_prefix} | sed 's/[[:space:]]\+/ /g' | cut -d ' ' -f 1 | while read namespace_name
 do
     echo_warning "kubectl delete namespace ${namespace_name}"
     kubectl delete namespace ${namespace_name}
 done
+
+# clean up docker images
+echo "################################################################################"
+echo_info "All docker images to delete:"
+az acr repository show-tags --name shootingstar --repository grpc-service-demo --output table > /dev/null 2>&1
+if [[ $? == 0 ]]; then
+    az acr repository show-tags --name shootingstar --repository grpc-service-demo --output table | tail -n +3 | awk '{print $1}' | grep "^pr-" | while read tag
+    do
+        echo_warning "grpc-service-demo:${tag}"
+        az acr repository delete --name shootingstar --image grpc-service-demo:${tag} --yes
+
+        echo_warning "grpc-client-demo:${tag}"
+        az acr repository delete --name shootingstar --image grpc-client-demo:${tag} --yes
+    done
+else
+    az acr repository show-tags --name shootingstar --repository grpc-client-demo --output table > /dev/null 2>&1
+    if [[ $? == 0 ]]; then
+        az acr repository show-tags --name shootingstar --repository grpc-client-demo --output table | tail -n +3 | awk '{print $1}' | grep "^pr-" | while read tag
+        do
+            echo_warning "grpc-client-demo:${tag}"
+            az acr repository delete --name shootingstar --image grpc-client-demo:${tag} --yes
+        done
+    fi
+fi
 
 echo ""
 echo_info "Done."
