140a322e2f47c70c66ffefc2ae5d86909ca2ba3c
zhanglue <zhanglue@gmail.com>
2025-03-13 08:36:54 +0000

    Add basic weather fetcher service as demo.

---
+++ src/weather_fetcher/weather_fetcher_service.h
@@ -0,0 +1,24 @@
+#ifndef WEATHER_FETCHER_SERVICE_H
+#define WEATHER_FETCHER_SERVICE_H
+
+#include <grpcpp/grpcpp.h>
+#include <unordered_map>
+
+#include "protos/weather_fetcher.grpc.pb.h"
+
+namespace weather_flow {
+
+class WeatherFetcherServiceImpl final : public WeatherFetcher::Service {
+ public:
+  WeatherFetcherServiceImpl();
+  grpc::Status GetWeather(grpc::ServerContext* context,
+                          const GetWeatherRequest* request,
+                          GetWeatherResponse* response) override;
+
+ private:
+  std::unordered_map<std::string, WeatherData::WeatherCondition> weather_data_map_;
+};
+
+}  // namespace weather_flow
+
+#endif  // WEATHER_FETCHER_SERVICE_H
